e7f8bb710e175b5c4450f395211d8a359a38744a
Amir Sharif <hmamirchishti@gmail.com>
2016-08-01 02:17:12 +0500

    Sendgrid has been implemented

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'amir_photo_application',
+    :password => 'Admin123',
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
 # Initialize the Rails application.
 Rails.application.initialize!
